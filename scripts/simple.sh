319232d98f85c88ae0f1f7f2473607af91635858
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-12-28 14:54:59 +0100

    simple test script for general go projects/folders

---
+++ scripts/simple.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export GOMUTESTING_DIFF=$(diff -u $MUTATE_ORIGINAL $MUTATE_CHANGED)
+
+mv $MUTATE_ORIGINAL $MUTATE_ORIGINAL.tmp
+cp $MUTATE_CHANGED $MUTATE_ORIGINAL
+
+go test ./... > /dev/null
+
+export GOMUTESTING_RESULT=$?
+
+mv $MUTATE_ORIGINAL.tmp $MUTATE_ORIGINAL
+
+case $GOMUTESTING_RESULT in
+0)
+	echo "$GOMUTESTING_DIFF"
+
+	exit 1
+	;;
+1)
+	exit 0
+	;;
+*) # Unkown exit code
+	exit $GOMUTESTING_RESULT
+	;;
+esac
