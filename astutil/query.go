0d15e9f06950cd7d352124c26ba180c203c6a588
Markus Zimmermann <markus.zimmermann@nethead.at>
2016-06-25 21:17:06 +0200

    Fix, try to make the noop-assignment a little less syntax breaking

--- astutil/query.go
+++ astutil/query.go
@@ -17,16 +17,36 @@ type identifierWalker struct {
 	identifiers []ast.Expr
 }
 
+var blacklistedIdentifiers = map[string]bool{
+	"_":      true,
+	"append": true,
+	"func":   true,
+	"nil":    true,
+}
+
+func checkForSelectorExpr(node ast.Expr) bool {
+	switch n := node.(type) {
+	case *ast.Ident:
+		return true
+	case *ast.SelectorExpr:
+		return checkForSelectorExpr(n.X)
+	}
+
+	return false
+}
+
 func (w *identifierWalker) Visit(node ast.Node) ast.Visitor {
 	switch n := node.(type) {
 	case *ast.Ident:
-		if n.Name != "nil" {
+		if _, ok := blacklistedIdentifiers[n.Name]; !ok {
 			w.identifiers = append(w.identifiers, n)
 		}
 
 		return nil
 	case *ast.SelectorExpr:
-		w.identifiers = append(w.identifiers, n)
+		if checkForSelectorExpr(n) {
+			w.identifiers = append(w.identifiers, n)
+		}
 
 		return nil
 	}
--- cmd/go-mutesting/main_test.go
+++ cmd/go-mutesting/main_test.go
@@ -15,7 +15,7 @@ func TestMain(t *testing.T) {
 		"../../example",
 		[]string{"--debug", "--exec-timeout", "1"},
 		returnOk,
-		"The mutation score is 0.538462 (7 passed, 6 failed, 1 skipped, total is 14)",
+		"The mutation score is 0.500000 (7 passed, 7 failed, 0 skipped, total is 14)",
 	)
 }
 
@@ -25,7 +25,7 @@ func TestMainRecursive(t *testing.T) {
 		"../../example",
 		[]string{"--debug", "--exec-timeout", "1", "./..."},
 		returnOk,
-		"The mutation score is 0.571429 (8 passed, 6 failed, 1 skipped, total is 15)",
+		"The mutation score is 0.533333 (8 passed, 7 failed, 0 skipped, total is 15)",
 	)
 }
 
@@ -35,7 +35,7 @@ func TestMainFromOtherDirectory(t *testing.T) {
 		"../..",
 		[]string{"--debug", "--exec-timeout", "1", "github.com/zimmski/go-mutesting/example"},
 		returnOk,
-		"The mutation score is 0.538462 (7 passed, 6 failed, 1 skipped, total is 14)",
+		"The mutation score is 0.500000 (7 passed, 7 failed, 0 skipped, total is 14)",
 	)
 }
 
