eb8e94d982738e0f3c5384145bddc2a298fcadbf
Markus Zimmermann <markus.zimmermann@nethead.at>
2018-08-04 12:30:40 +0200

    Let our linters ignore the example directory

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,19 +4,19 @@ if [ -z ${PKG+x} ]; then echo "PKG is not set"; exit 1; fi
 if [ -z ${ROOT_DIR+x} ]; then echo "ROOT_DIR is not set"; exit 1; fi
 
 echo "gofmt:"
-OUT=$(gofmt -l $ROOT_DIR)
+OUT=$(gofmt -l $ROOT_DIR 2>&1 | grep --invert-match -E "(/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 echo "errcheck:"
-OUT=$(errcheck $PKG/...)
+OUT=$(errcheck $PKG/... 2>&1 | grep --invert-match -E "(/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 echo "go vet:"
-OUT=$(go tool vet -all=true -v=true $ROOT_DIR 2>&1 | grep --invert-match -E "(Checking file|\%p of wrong type|can't check non-constant format)")
+OUT=$(go tool vet -all=true -v=true $ROOT_DIR 2>&1 | grep --invert-match -E "(Checking file|\%p of wrong type|can't check non-constant format|/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 echo "golint:"
-OUT=$(golint $PKG/...)
+OUT=$(golint $PKG/... 2>&1 | grep --invert-match -E "(/example)")
 if [ -n "$OUT" ]; then echo "$OUT"; PROBLEM=1; fi
 
 if [ -n "$PROBLEM" ]; then exit 1; fi
