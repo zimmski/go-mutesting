88a05ba1a0f609fef614b43e8edd7a769fd0a5a2
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-08-18 18:26:26 +0100

    Add comparison expression mutator

---
+++ testdata/expression/comparison.go.0.go
@@ -0,0 +1,27 @@
+// +build example-main
+
+package main
+
+import "fmt"
+
+func main() {
+	if 1 >= 2 {
+		fmt.Printf("1 is greater than 2!")
+	}
+
+	if 1 < 2 {
+		fmt.Printf("1 is less than 2!")
+	}
+
+	if 1 <= 2 {
+		fmt.Printf("1 is less than or equal to 2!")
+	}
+
+	if 1 >= 2 {
+		fmt.Printf("1 is greater than or equal to 2!")
+	}
+
+	if 1 == 2 {
+		fmt.Print("1 is equal to 2!")
+	}
+}
